3fdb42cbd881cef1fb84095aaac5bc11ea99f3ca
eren5 <eren@heybooster.ai>
2021-09-21 18:24:23 +0300

    New Version Publish

--- publish.sh
+++ publish.sh
@@ -6,5 +6,5 @@ python3 -m build
 python3 -m pip install --upgrade twine
 python3 -m twine upload --repository pypi dist/*
 git add .
-git commit -m " New Version Publish"
+git commit -m "New Version Publish"
 git push -f
--- setup.py
+++ setup.py
@@ -24,5 +24,8 @@ setuptools.setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
+    install_requires=[
+        'pymongo==3.10.1'
+    ],
     python_requires=">=3.6",
 )
