baa6eeda0125bb53bb074aaac03537576a1438fa
selamet <selamet96@gmail.com>
2023-06-12 18:57:16 +0300

    Create get methods payload

--- heybooster/helpers/database/opensearch.py
+++ heybooster/helpers/database/opensearch.py
@@ -86,13 +86,31 @@ class OpenSearchHelper:
         This function returns search response
         """
         try:
-            params = [f"{key}:{value}" for key, value in kwargs.items()]
-            path = f"_search?q={','.join(params)}&size={size}&sort={sort}"
+            path = f"_search"
             url = self.get_url(path=path)
+            sort_key = sort.split(":")[0]
+            sort_order = sort.split(":")[1]
+
+            payload = {
+                "sort": [
+                    {
+                        sort_key: {
+                            "order": sort_order
+                        }
+                    }
+                ],
+                "size": size,
+                "query": {
+                    "match": {
+                        **kwargs
+                    }
+                }
+            }
 
             response = self.__perform_request(
                 method=OpenSearchHelper.GET,
                 url=url,
+                payload=payload,
             )
 
             return response
